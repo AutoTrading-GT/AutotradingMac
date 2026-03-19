69b225252fb3336d1d027cd1314bfb684ec1ca8b
hahn56789 <hahn56789@gmail.com>
2026-03-19 14:54:35 +0900

    Initial Commit

---
+++ AutotradingMac/AutotradingMacApp.swift
@@ -0,0 +1,17 @@
+//
+//  AutotradingMacApp.swift
+//  AutotradingMac
+//
+//  Created by 한규탁 on 3/19/26.
+//
+
+import SwiftUI
+
+@main
+struct AutotradingMacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
